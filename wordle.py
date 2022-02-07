8aaa50646acd0cd89586fd91a4a39523c9b0cfc0
everyday-special <mlidell@gmail.com>
2022-02-07 09:20:40 -0800

    Can now play multiple rounds

--- wordle.py
+++ wordle.py
@@ -1,39 +1,10 @@
-"""import os
-
-from secret_word import secretWord
-from string import ascii_lowercase
-from termcolor import colored
+from wordle_ui import ui
 
 def main():
-    guesses = []
-    secret_word = secretWord()
-    clear()
-    for i in range(6):
-        guess = input("Enter your guess:\n").lower()
-        colors = secret_word.checkGuess(guess)
-        guesses.append((guess, colors))
-        clear()
-        for g in guesses:
-            printGuess(g[0], g[1])
-        if colors == ['green'] * 5:
-            print("\nyou win!")
-            break
-    print(f'\n{secret_word.word}')
-
-
-def printGuess(guess, colors):
-    for i, ch in enumerate(guess):
-        print(colored(ch, colors[i]), end=' ')
-    print()
-
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')"""
-from wordle_gui import gui
-
-def main():
-    wordle_gui = gui()
-    wordle_gui.main()
+    wordle = ui()
+    play = True
+    while play:
+        play = wordle.main()
 
 
 if __name__ == '__main__':
--- wordle_ui.py
+++ wordle_ui.py
@@ -8,15 +8,11 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-class gui:
+class ui:
     def __init__(self, allowed_words_file='allowed_guesses.txt'):
         with open(allowed_words_file, 'r') as file:
             self.allowed_words = [line.rstrip() for line in file]
-        self.word = secretWord()
         self.letterbank = {}
-        self.guesses = []
-        for ch in ascii_lowercase:
-            self.letterbank.update({ch: 'white'})
 
     def printGuesses(self):
         clear()
@@ -39,7 +35,7 @@ class gui:
             print(colored(letter, self.letterbank[letter]), end=' ')
         print()
 
-    def getInput(self):
+    def getGuess(self):
         guess = input('Enter your guess: ').lower()
         while guess not in self.allowed_words:
             clear()
@@ -47,14 +43,31 @@ class gui:
             guess = input('Invalid word. Please enter a valid five letter word: ').lower()
         return guess
 
+    def playAgain(self):
+        response = input('Do you want to play again? (y/n): ')
+        while response not in 'yn':
+            response = input('Please enter "y" or "n" to indicate whether you want to play again: ')
+        return response == 'y'
+
+    def reset(self):
+        self.word = secretWord()
+        self.guesses = []
+        for ch in ascii_lowercase:
+            self.letterbank.update({ch: 'white'})
+
     def main(self):
+        self.reset()
         clear()
+        win = False
         for i in range(6):
-            guess = self.getInput()
+            guess = self.getGuess()
             colors = self.word.checkGuess(guess)
             self.guesses.append((guess, colors))
             self.printGuesses()
             if colors == ['green'] * 5:
                 print("You win!")
+                win = True
                 break
-        print(f'\n{self.word.word}')
+        if not win:
+            print(f'\nThe word was "{self.word.word}"')
+        return self.playAgain()
